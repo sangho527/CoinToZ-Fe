0b8510f0695622e70d2b4a5f9bb3bd42338394dc
DongYeon Kang <myjjue00@gmail.com>
2023-02-06 16:21:19 +0900

    Feat : mainPage에 SIdeBar 추가

---
+++ src/components/pages/UpbitMainPage/UpbitMainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SideMenu from '../../common/SideMenu/SideMenu';
+
+function UpbitMainPage() {
+    return (
+        <>
+        <SideMenu/>
+        </>
+    );
+
+}
+export default UpbitMainPage;
