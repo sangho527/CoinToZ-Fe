14edcb762d38ef3de588d5dfec71e0b15c3b362a
DongYeon Kang <myjjue00@gmail.com>
2023-02-07 02:40:27 +0900

    Feat : upbit QuotationAPI UI 띄우기

--- src/App.js
+++ src/App.js
@@ -12,7 +12,17 @@ import UserModifyForm from './components/pages/UserInfoModify/UserModifyForm';
 import PasswordModiftForm from './components/pages/UserInfoModify/PasswordModifyForm';
 import UpbitMainPage from './components/pages/UpbitMainPage/UpbitMainPage';
 import Dashboard from './components/pages/Diary/Dashboard';
-
+import NavRestApi from './NavRestApi';
+import MarketCode from './QuotationAPI/RESTAPI-example/MarketCode';
+import DayCandleData from './QuotationAPI/RESTAPI-example/DayCandleData';
+import MinuteCandleData from './QuotationAPI/RESTAPI-example/MinuteCandleData';
+import WeekMonthCandleData from './QuotationAPI/RESTAPI-example/WeekMonthCandleData';
+import TradeHistoryData from './QuotationAPI/RESTAPI-example/TradeHistoryData';
+import NavWebsocketApi from './NavWebsocketApi';
+import OrderBook from 'upbit-api/container/OrderBook';
+import RealTimePrice from './QuotationAPI/WEBSOCKET-example/RealTimePrice';
+import TradeHistory from './QuotationAPI/WEBSOCKET-example/TradeHistory';
+import TotalExample from './QuotationAPI/TOTAL-example/TotalExample';
 
 function App() {
 
@@ -31,7 +41,25 @@ function App() {
                     <Route path='/mypage/password/modify' element={<PasswordModiftForm/>} />
                     <Route path='/upbitMainPage' element={<UpbitMainPage/>}/>
                     <Route path='/diary' element={<Dashboard/>} />
-                </Routes>
+                    <Route path="rest-api" element={<NavRestApi />}>
+                        <Route path="marketcode" element={<MarketCode />} />
+                        <Route path="day-candle-data" element={<DayCandleData />} />
+                        <Route path="minute-candle-data" element={<MinuteCandleData />} />
+                        <Route path="week-month-candle-data" element={<WeekMonthCandleData />} />
+                        <Route path="trade-history-data" element={<TradeHistoryData />} />
+                    </Route>
+                    <Route path="websocket-api" element={<NavWebsocketApi />}>
+                        <Route path="order-book" element={<OrderBook />} />
+                        <Route path="real-time-price" element={<RealTimePrice />} />
+                        <Route path="trade-history" element={<TradeHistory />} />
+                    </Route>
+                    <Route
+                        path="total-example"
+                        element={
+                        <RecoilRoot>
+                            <TotalExample />
+                        </RecoilRoot>}/>
+                    </Routes>
             </RecoilRoot>
         </>
     );
