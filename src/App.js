8a4bd0e018d61b334d11fe3dd91c57bab8f2c835
ohy1023 <zvyg1023@naver.com>
2023-02-06 22:45:59 +0900

    feat : 매매일지 중간 커밋

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Mypage from './components/pages/Mypage/Mypage';
 import PasswordValidation from './components/pages/Validation/PasswordValidation';
 import UserModifyForm from './components/pages/UserInfoModify/UserModifyForm';
 import PasswordModiftForm from './components/pages/UserInfoModify/PasswordModifyForm';
+import Dashboard from './components/pages/Diary/Dashboard';
 
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
                     <Route path='/mypage/password/validation' element={<PasswordValidation/>}/>
                     <Route path='/mypage/info/modify' element={<UserModifyForm/>}/>
                     <Route path='/mypage/password/modify' element={<PasswordModiftForm/>} />
+                    <Route path='/diary' element={<Dashboard/>} />
                 </Routes>
             </RecoilRoot>
         </>
