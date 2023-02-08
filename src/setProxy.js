cab8b3c534e8018161fe83690e364250005a667c
강수빈 <tnbin98@gmail.com>
2023-02-08 16:02:27 +0900

    Fix:nginx 파일 수정

--- src/setProxy.js
+++ src/setProxy.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     '/api', // 프론트엔드에서 '/api'로 요청을 보내면, 백엔드인 8080포트(=target)로 요청이 도착
     createProxyMiddleware({
       target: 'http://ec2-52-78-23-203.ap-northeast-2.compute.amazonaws.com:8080',	// 서버 URL or localhost:설정한 포트번호
-      //target:'http://localhost:8080',
+     // target:'http://localhost:8080',
       changeOrigin: true,
     })
   );
