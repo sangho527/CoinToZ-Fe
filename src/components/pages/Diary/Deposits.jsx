66602fb5e56a4eaa03990d5646562bffe4604851
ohy1023 <zvyg1023@naver.com>
2023-02-12 19:25:00 +0900

    factor : 매매일지

--- src/components/pages/Diary/Deposits.jsx
+++ src/components/pages/Diary/Deposits.jsx
@@ -8,13 +8,13 @@ import Api from '../../../functions/customApi';
 export default function Deposits() {
 
   const navigate = useNavigate();
-  let [sumEtcCnt, setSumEtcCnt] = React.useState(0);
-  const [bitCoinCnt, setBitCoinCnt] = React.useState(0);
-  const [ethereumCnt, setEthereumCnt] = React.useState(0);
-  const [rippleCnt, setRippleCnt] = React.useState(0);
-  const [ADACnt, setADACnt] = React.useState(0);
-  const [dogeCoinCnt, setDogeCoinCnt] = React.useState(0);
-  const [etcCnt, setEtcCnt] = React.useState(0);
+  let [sumEtcCnt, setSumEtcCnt] = React.useState(0.0);
+  const [bitCoinCnt, setBitCoinCnt] = React.useState(0.0);
+  const [ethereumCnt, setEthereumCnt] = React.useState(0.0);
+  const [rippleCnt, setRippleCnt] = React.useState(0.0);
+  const [ADACnt, setADACnt] = React.useState(0.0);
+  const [dogeCoinCnt, setDogeCoinCnt] = React.useState(0.0);
+  const [etcCnt, setEtcCnt] = React.useState(0.0);
 
 
   const getCoinCount = async () => {
@@ -35,22 +35,22 @@ export default function Deposits() {
 
   function settingCnt(data) {
     if (data.currency === "XRP" && data.unit_currency === "KRW") {
-      setRippleCnt(data.balance);
+      setRippleCnt(parseFloat(data.balance));
     }
     else if (data.currency === "BTC" && data.unit_currency === "KRW") {
-      setBitCoinCnt(data.balance);
+      setBitCoinCnt(parseFloat(data.balance));
     }
     else if (data.currency === "ETH" && data.unit_currency === "KRW") {
-      setEthereumCnt(data.balance);
+      setEthereumCnt(parseFloat(data.balance));
     }
     else if (data.currency === "ADA" && data.unit_currency === "KRW") {
-      setADACnt(data.balance);
+      setADACnt(parseFloat(data.balance));
     }
     else if (data.currency === "DOGE" && data.unit_currency === "KRW") {
-      setDogeCoinCnt(data.balance);
+      setDogeCoinCnt(parseFloat(data.balance));
     }
     else if (data.currency !== "KRW"){
-      sumEtcCnt = sumEtcCnt + data.balance;
+      sumEtcCnt = sumEtcCnt + parseFloat(data.balance);
     }
   }
 
@@ -59,37 +59,37 @@ export default function Deposits() {
     {
       "id": "비트코인",
       "label": "비트코인",
-      "value": bitCoinCnt,
+      "value": bitCoinCnt.toFixed(2),
       "color": "hsl(309, 70%, 50%)"
     },
     {
       "id": "이더리움",
       "label": "이더리움",
-      "value": ethereumCnt,
+      "value": ethereumCnt.toFixed(2),
       "color": "hsl(124, 70%, 50%)"
     },
     {
       "id": "리플",
       "label": "리플",
-      "value": rippleCnt,
+      "value": rippleCnt.toFixed(2),
       "color": "hsl(96, 70%, 50%)"
     },
     {
       "id": "에이다",
       "label": "에이다",
-      "value": ADACnt,
+      "value": ADACnt.toFixed(2),
       "color": "hsl(204, 70%, 50%)"
     },
     {
       "id": "도지코인",
       "label": "도지코인",
-      "value": dogeCoinCnt,
+      "value": dogeCoinCnt.toFixed(2),
       "color": "hsl(115, 70%, 50%)"
     },
     {
       "id": "기타",
       "label": "기타",
-      "value": etcCnt,
+      "value": etcCnt.toFixed(2),
       "color": "hsl(246, 100%, 50%)"
     }
   ];
@@ -150,11 +150,6 @@ export default function Deposits() {
         ]}
 
       />
-      <div>
-        <Link style={{ float: 'right' }} color="primary" onClick={() => navigate('/test')}>
-          자세히 보기
-        </Link>
-      </div>
     </React.Fragment>
   );
 }
