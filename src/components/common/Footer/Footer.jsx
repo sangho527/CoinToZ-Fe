aabc5ff5f45c94ddedae2ede77b4f82dc5210393
DongYeon Kang <myjjue00@gmail.com>
2023-02-15 05:23:03 +0900

    Feat : header, footer 수정

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import { AiFillGithub } from 'react-icons/ai';
 import logo from '../../../assets/appBar/logo.png';
 import styled from 'styled-components';
 import { css } from 'styled-components';
+import { Col, Row } from 'antd';
 
 const container = css`
 	max-width: 1400px;
@@ -14,18 +15,18 @@ const container = css`
 
 const FooterComponent = styled.footer`
 	width: 100%;
-	height: 130px;
+	height: 180px;
 	display: flex;
 	justify-content: center;
-	background: #D9D0F6;
+	background: #e2e2e2;
 	.content {
 		${container}
 		display: flex;
-		align-items: center;
-		justify-content: space-between;
+		// align-items: center;
+		// justify-content: space-between;
 		.rep {
 			display: flex;
-			align-items: center;
+			// align-items: center;
 			> div {
 				:nth-child(1) {
 					padding: 0 0.5rem;
@@ -34,10 +35,10 @@ const FooterComponent = styled.footer`
 		}
 		a {
 			display: flex;
-			justify-content: center;
+			// justify-content: center;
 			padding: 0.125rem 0.125rem;
-			align-items: center;
-			text-decoration: none;
+			align-items: left;
+			font-style: italic;
 			margin: 0.125rem 0.25rem;
 			color: #333;
 			:active,
@@ -47,7 +48,7 @@ const FooterComponent = styled.footer`
 			}
 		}
 		img {
-			width: 115px;
+			width: 110px;
 			height: 80px;
 		}
 		> div {
@@ -70,14 +71,14 @@ function Footer() {
     repository2: 'https://gitlab.com/people-of-development1/financial-final-project-fe',
     copyright: 'CoinToZ © 2023 All rights reserved.',
     be1: [
-      { name: '강동연', repository: 'https://github.com/dongyeon-0822' },
-      { name: '강수빈', repository: 'https://github.com/kang-subin' },
-      { name: '김상호', repository: 'https://github.com/sangho527' },
+      { name: 'Kang DongYeon', repository: 'https://github.com/dongyeon-0822' },
+      { name: 'Kang Subin', repository: 'https://github.com/kang-subin' },
+      { name: 'Kim Sangho', repository: 'https://github.com/sangho527' },
     ],
     be2: [
-      { name: '손세열', repository: 'https://github.com/ccodding' },
-      { name: '오형상', repository: 'https://github.com/ohy1023' },
-      { name: '임학준', repository: 'https://github.com/hakjun1' },
+      { name: 'Son seyeol', repository: 'https://github.com/ccodding' },
+      { name: 'OHyungSang', repository: 'https://github.com/ohy1023' },
+      { name: 'Yim Hakjun', repository: 'https://github.com/hakjun1' },
     ],
   };
   const navigate = useNavigate();
@@ -85,40 +86,55 @@ function Footer() {
   return (
     <FooterComponent>
       <div className="content">
-        <div>
-          <img src={logo} onClick={() => navigate('/')} alt=""/>
-          <a href={FOOTER_LIST.repository1} target="_blank" rel="noreferrer">
-            BackEnd GitLab Repository
-          </a>
-          <a href={FOOTER_LIST.repository2} target="_blank" rel="noreferrer">
-            FrontEnd GitLab Repository
-          </a>
-        </div>
-        <div>{FOOTER_LIST.copyright}</div>
-        <div>
-          <div className="rep">
-            <div>BE</div>
-            {FOOTER_LIST.be1.map((v, i) => (
-              <a href={v.repository} key={i} target="_blank" rel="noreferrer">
-                <div>
-                  <AiFillGithub />
-                </div>
-                <div>{v.name}</div>
-              </a>
-            ))}
+      <Row style={{width : '100%'}}>
+        <Col span={6}>
+          <div>
+            <Row><h5>ABOUT</h5></Row>
+            <ul>
+              <li>
+                <a href={FOOTER_LIST.repository1} target="_blank" rel="noreferrer">
+                  [BackEnd] GitLab Repository
+                </a>
+              </li>
+              <li>
+                <a href={FOOTER_LIST.repository2} target="_blank" rel="noreferrer">
+                  [FrontEnd] GitLab Repository
+                </a>
+              </li>
+            </ul>
           </div>
-          <div className="rep">
-            <div>BE</div>
-            {FOOTER_LIST.be2.map((v, i) => (
-              <a href={v.repository} key={i} target="_blank" rel="noreferrer">
-                <div>
-                  <AiFillGithub />
-                </div>
-                <div>{v.name}</div>
-              </a>
-            ))}
+        </Col>
+        
+        <Col span={12}>
+          <div>
+            <Row><h5>DEVELOPER</h5></Row>
+            <div className="rep">
+              {FOOTER_LIST.be1.map((v, i) => (
+                <a href={v.repository} key={i} target="_blank" rel="noreferrer">
+                  <div>
+                    <AiFillGithub />
+                  </div>
+                  <div>{v.name}</div>
+                </a>
+              ))}
+            </div>
+            <div className="rep">
+              {FOOTER_LIST.be2.map((v, i) => (
+                <a href={v.repository} key={i} target="_blank" rel="noreferrer">
+                  <div>
+                    <AiFillGithub />
+                  </div>
+                  <div>{v.name}</div>
+                </a>
+              ))}
+            </div>
           </div>
-        </div>
+        </Col>
+
+        <Col span={4}>
+          <div style={{color : '#757575', marginTop: '90px'}}>{FOOTER_LIST.copyright}</div>
+        </Col>
+      </Row>
       </div>
     </FooterComponent>
   );
